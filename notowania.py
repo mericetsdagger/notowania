d41b6af6106756b267e53f83bfdc8499a02511e2
mericetsdagger <45637254+mericetsdagger@users.noreply.github.com>
2019-01-05 22:27:18 +0100

    Update notowania.py

--- notowania.py
+++ notowania.py
@@ -11,8 +11,10 @@ def not_allowed(insert_statement):
 def split_txt_to_insert(podaj_wyciag, tabela, a1nazwa):
     lista = podaj_wyciag.split("\n")
     if lista[-1] == "":
-      del lista[-1]
+      del lista[-1] 
+    lista_fix = []
     liczba_przecinkow = lista[0].count(",")
+    a_wyc = podaj_wyciag
     a_insert_into = tabela
     a_kolumna1 = a1nazwa
     a_1 = ""
@@ -26,58 +28,63 @@ def split_txt_to_insert(podaj_wyciag, tabela, a1nazwa):
     with open("notowania_omitted.txt","r") as f:
         o_text = f.read()
     omitted = o_text.split("\n")
-    del omitted[-1]
-
+  
     with open("lista_wig.txt","r") as e:
         l_text = e.read()
     wigi = l_text.split("\n")
 
-### zmiana if do obdlugi wig
-    for i in lista:
-        
+    if a_wyc == notowania:
+        if a_insert_into == "NOTOWANIA":
+            for a in lista:
+                if a[0:a.find(",")] not in omitted:
+                    lista_fix.append(a)
+           ### ogarnac indeksy wykluczone 
+        elif tabela == "WIG":
+            for a in lista:
+                if a[0:a.find(",")] in wigi:
+                    lista_fix.append(a)
+
+    else:
+        lista_fix = lista.copy()
+
+
+    for i in lista_fix:
+        print(i)
         przecinek = i.find(",")
-        a_1 = i[0:przecinek]
-        if a_1 in omitted:
-            lista.remove(i)
-            
-        else:    
-            przecinek = i.find(",",przecinek)
-            a_data = i[przecinek+1:i.find(",",przecinek+1)]
-            przecinek = i.find(",",przecinek+1)
-            a_otwarcie = i[przecinek+1:i.find(",",przecinek+1)]
-            przecinek = i.find(",",przecinek+1)
-            a_maks = i[przecinek+1:i.find(",",przecinek+1)]
-            przecinek = i.find(",",przecinek+1)
-            a_min = i[przecinek+1:i.find(",",przecinek+1)]
-            przecinek = i.find(",",przecinek+1)
-            a_zamkniecie = i[przecinek+1:i.find(",",przecinek+1)]
+        a_1 = i[0:przecinek]    
+        przecinek = i.find(",",przecinek)
+        a_data = i[przecinek+1:i.find(",",przecinek+1)]
+        przecinek = i.find(",",przecinek+1)
+        a_otwarcie = i[przecinek+1:i.find(",",przecinek+1)]
+        przecinek = i.find(",",przecinek+1)
+        a_maks = i[przecinek+1:i.find(",",przecinek+1)]
+        przecinek = i.find(",",przecinek+1)
+        a_min = i[przecinek+1:i.find(",",przecinek+1)]
+        przecinek = i.find(",",przecinek+1)
+        a_zamkniecie = i[przecinek+1:i.find(",",przecinek+1)]
 
-            a_insert = a_insert + ("\n ('{}',str_to_date('{}','%Y%m%d'), {}, {}, {}, {})").format(a_1,
-                                                                                            a_data,
-                                                                                            a_otwarcie,
-                                                                                            a_maks,
-                                                                                            a_min,
-                                                                                            a_zamkniecie)
-            if i != lista[-1]:
-                a_insert = a_insert + ","
-
+        a_insert += ("\n ('{}',str_to_date('{}','%Y%m%d'), {}, {}, {}, {})").format(a_1,
+                                                                                    a_data,
+                                                                                    a_otwarcie,
+                                                                                    a_maks,
+                                                                                    a_min,
+                                                                                    a_zamkniecie)
+        if i != lista_fix[-1]:
+            a_insert += ","
         
-    if a_insert[-1] == ",":
-        a_insert = a_insert[:-1]
     return a_insert
         
 #część odpowiadająca za pobranie danych z polskiej gieldy i przerobienie na sql
 notowania = requests.get("http://bossa.pl/pub/ciagle/omega/cgl/ndohlcv.txt").text
-######### dodać podział na indeksy i spółki
-sql_notowania = split_txt_to_insert(notowania, "NOTOWANIA", "SPOLKA")
-sql_wig = split_txt_to_insert(notowania,"WIG","INDEX")
+#sql_notowania = split_txt_to_insert(notowania, "NOTOWANIA", "SPOLKA")
+#sql_wig = split_txt_to_insert(notowania,"WIG","INDEX")
 #część odpowiadająca za pobranie danych z giełd światowych i przerobienie na sql
 strona_zagranica = requests.get("http://bossa.pl/pub/indzagr/mstock/sesjazgr/sesjazgr.prn").text
 sql_zagranica = split_txt_to_insert(strona_zagranica, "GIELDY", "GIELDA")
-
+print(sql_zagranica)
 #część odpowiadająca za zaciągnięcie do bazy danych walut        
-strona_waluty = requests.get("http://bossa.pl/pub/waluty/mstock/sesjanbp/sesjanbp.prn").text
-sql_waluty = split_txt_to_insert(strona_waluty, "WALUTY", "WALUTA")
+#strona_waluty = requests.get("http://bossa.pl/pub/waluty/mstock/sesjanbp/sesjanbp.prn").text
+#sql_waluty = split_txt_to_insert(strona_waluty, "WALUTY", "WALUTA")
 '''
 #pakowanie do bazy
 connection = pymysql.connect(host = "host",
@@ -95,4 +102,3 @@ connection.commit()
 cur.close()
 connection.close()
 '''
-
