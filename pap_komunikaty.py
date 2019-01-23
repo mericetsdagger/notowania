41ea7acaabf43bfb8008ca77df55a68ed50d96ef
mericetsdagger <45637254+mericetsdagger@users.noreply.github.com>
2019-01-23 22:13:43 +0100

    Update pap_komunikaty.py

--- pap_komunikaty.py
+++ pap_komunikaty.py
@@ -2,14 +2,20 @@ import requests
 from DBCon import DBCon
 from datetime import datetime, timedelta
 
+dzis = datetime.today().strftime('%Y-%m-%d').replace("-",".") 
 wczoraj = (datetime.today()-timedelta(days=1)).strftime('%Y-%m-%d').replace("-",".")
 przedwcz = (datetime.today()-timedelta(days=2)).strftime('%Y-%m-%d').replace("-",".")
+wskaz_dzis = """<td colspan="2" class="dni">"""+dzis+"</td>"
 wskaz_dzien = """<td colspan="2" class="dni">"""+wczoraj+"</td>"
 wskaz_pwcz = """<td colspan="2" class="dni">"""+przedwcz+"</td>"
 kom_list = []
 a_sql = ""
 
 for i in range(1,20):
+    #wybranie do obslugi tylko stron zawierajacych:
+    #1. Daty dzisiejszej - pomijamy
+    #2. Daty wczorajszej - zczytujemy w listę
+    #2. Daty wczorajszej i przedwczorajszej - zczytujemy do pojawienia się daty przedwczorajszej
     print(i)
     strona = ("http://biznes.pap.pl/pl/news/listings/{},").format(i)
     komunikat = requests.get(strona).text
@@ -20,14 +26,18 @@ for i in range(1,20):
             kom = komunikat[komunikat.find(wskaz_dzien):komunikat.find(wskaz_pwcz)]
 
         else:
-            kom = komunikat[komunikat.find(wskaz_dzien):komunikat.find("""<div class="stronicowanie">""")]        
+            kom = komunikat[komunikat.find(wskaz_dzien):komunikat.find("""<div class="stronicowanie">""")]
+        kom = kom.strip()    
         kom_list = kom_list + kom.split("""<td class="wdgodz">""")
     else:
-        pass
-    
+        if wskaz_dzis in komunikat:
+            pass
+        else:
+            break
+    print(len(kom_list))
 intt = 0
 for i in kom_list:
-    if i[0:28] == """<td colspan="2" class="dni">""":
+    if i[0:28] != """<td colspan="2" class="dni">""":
         link = "www.biznes.pap.pl" + i[i.find("""/pl/news/listing"""):i.find('''"''',i.find("""/pl/news/listing""")+1)]
         tresc = i[i.find("""style="">""")+9:i.find("""</a>""")]
         czas_pub = wczoraj + " " + i[0:5]
@@ -36,8 +46,3 @@ for i in kom_list:
         intt = intt+1
     else:
         pass
-    
-    
-    
-
-
